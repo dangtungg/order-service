914afc2abc48bd240c1f9ef27a1b6bd649061b78
Tung Dang <dtt.dangthanhtung@gmail.com>
2024-11-15 17:15:54 +0700

    setup observability & monitoring

--- src/main/java/com/example/order_service/order/web/OrderController.java
+++ src/main/java/com/example/order_service/order/web/OrderController.java
@@ -3,6 +3,8 @@ package com.example.order_service.order.web;
 import com.example.order_service.order.domain.Order;
 import com.example.order_service.order.service.OrderService;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +15,7 @@ import reactor.core.publisher.Mono;
 @RequestMapping("/orders")
 public class OrderController {
 
+    private static final Logger log = LoggerFactory.getLogger(OrderController.class);
     private final OrderService orderService;
 
     public OrderController(OrderService orderService) {
@@ -21,11 +24,13 @@ public class OrderController {
 
     @GetMapping
     public Flux<Order> getAllOrders(@AuthenticationPrincipal Jwt jwt) {
+        log.info("Fetching all orders");
         return orderService.getAllOrders(jwt.getSubject());
     }
 
     @PostMapping
     public Mono<Order> submitOrder(@RequestBody @Valid OrderRequest orderRequest) {
+        log.info("Order for {} copies of the book with ISBN {}", orderRequest.quantity(), orderRequest.isbn());
         return orderService.submitOrder(orderRequest.isbn(), orderRequest.quantity());
     }
 
